d765dd7b682b6472f15f09464818f6c0040fb33d
chuck.f.willis <chuck.f.willis@0a5599c8-c0cf-11de-a250-c7f789e21d83>
2010-03-24 01:58:44 +0000

    Updating docs and misc other stuff.

--- trunk/var/www/peruggia/includes/main.php
+++ trunk/var/www/peruggia/includes/main.php
@@ -17,7 +17,7 @@
  * 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-$images = array_diff(scandir("images"), array(".", ".."));
+$images = array_diff(scandir("images"), array(".", "..", ".svn"));
 
 if(sizeof($images) == 0){
 
