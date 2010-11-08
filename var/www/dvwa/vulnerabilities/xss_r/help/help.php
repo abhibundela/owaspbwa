f97625597c081d690b2d16ec46ea43d06edace76
chuck.f.willis <chuck.f.willis@0a5599c8-c0cf-11de-a250-c7f789e21d83>
2010-11-08 22:20:37 +0000

    Removed DVWA 1.06 to make room for version 1.07

--- var/www/dvwa/vulnerabilities/xss_r/help/help.php
+++
@@ -1,5 +0,0 @@
-<h1>Help - Cross Site Scripting (XSS)</h1>
-
-		<p>The 'name' variable within this PHP script is vulnerable to Cross Site Scripting.</p>
-		<p>You can steal cookies, deface and much more...</p>
-		<p>Example: http://127.0.0.1/dvwa/xss.php?name=javascript</p>
