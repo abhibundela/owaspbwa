d96ab2f83de55ec2e49b091f84ab994e42a9a467
chuck.f.willis@gmail.com <chuck.f.willis@gmail.com@0a5599c8-c0cf-11de-a250-c7f789e21d83>
2011-01-17 07:39:49 +0000

    Updated scripts to display a message to indicate what is being reconfigured.

--- trunk/usr/local/bin/owaspbwa-modsecurity-crs-log.sh
+++ trunk/usr/local/bin/owaspbwa-modsecurity-crs-log.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+echo "\nConfiguring Apache with the ModSecurity Core Rule Set configured to log (but not block) bad requests.  Log messages appear in /var/log/apache2/error.log\n"
 rm /etc/apache2/apache2.conf
 ln -s /etc/apache2/apache2-modsecurity-crs-log.conf /etc/apache2/apache2.conf
 /etc/init.d/apache2 restart
