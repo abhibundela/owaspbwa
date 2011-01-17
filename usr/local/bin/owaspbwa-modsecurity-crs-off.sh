5d5cb322475c270c55f601893f805ba7ec26b281
chuck.f.willis <chuck.f.willis@gmail.com>
2011-01-17 07:39:49 +0000

    Updated scripts to display a message to indicate what is being reconfigured.

--- usr/local/bin/owaspbwa-modsecurity-crs-off.sh
+++ usr/local/bin/owaspbwa-modsecurity-crs-off.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+echo "\nConfiguring Apache with the ModSecurity Core Rule Set disabled.  No logging or blocking of bad requests will occur.\n"
 rm /etc/apache2/apache2.conf
 ln -s /etc/apache2/apache2-modsecurity-crs-off.conf /etc/apache2/apache2.conf
 /etc/init.d/apache2 restart
