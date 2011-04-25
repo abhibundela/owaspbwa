1cd74e56065f589e549ab8f9953920979df20d29
michaeltcyr@gmail.com <michaeltcyr@gmail.com@0a5599c8-c0cf-11de-a250-c7f789e21d83>
2011-04-25 10:54:09 +0000

    Update per issue 25

--- trunk/usr/local/bin/owaspbwa-services-start.sh
+++ trunk/usr/local/bin/owaspbwa-services-start.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-/etc/init.d/mysql start
-/etc/init.d/tomcat6 start
-/etc/init.d/apache2 start
+/usr/sbin/service mysql start
+#/etc/init.d/mysql start
+/usr/sbin/service tomcat6 start
+#/etc/init.d/tomcat6 start
+/usr/sbin/service apache2 start
+#/etc/init.d/apache2 start
 
--- trunk/usr/local/bin/owaspbwa-services-stop.sh
+++ trunk/usr/local/bin/owaspbwa-services-stop.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-/etc/init.d/mysql stop
-/etc/init.d/tomcat6 stop
-/etc/init.d/apache2 stop
+/usr/sbin/service mysql stop
+#/etc/init.d/mysql stop
+/usr/sbin/service tomcat6 stop
+#/etc/init.d/tomcat6 stop
+/usr/sbin/service apache2 stop
+#/etc/init.d/apache2 stop
 
