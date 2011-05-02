511449f08bee60561ae2c5b7dc3c635b44c9a405
michaeltcyr <michaeltcyr@gmail.com>
2011-05-02 15:13:37 +0000

    Updated owaspbwa-svn-update.sh to fix file permissions on other .sh scripts

--- usr/local/bin/owaspbwa-svn-update.sh
+++ usr/local/bin/owaspbwa-svn-update.sh
@@ -4,4 +4,8 @@ owaspbwa-services-stop.sh
 echo "---- Updating from SVN ----"
 svn update --accept theirs-full /owaspbwa/owaspbwa-svn
 echo "---- Fixing file permissions and restarting services ----"
+#when we update this filee, it breaks the permmissions
+chmod +x /owaspbwa/owaspbwa-svn/usr/local/bin/owaspbwa-fix-file-permissions.sh
+chmod +x /owaspbwa/owaspbwa-svn/usr/local/bin/owaspbwa-services-stop.sh
+chmod +x /owaspbwa/owaspbwa-svn/usr/local/bin/owaspbwa-services-start.sh
 owaspbwa-fix-file-permissions.sh
