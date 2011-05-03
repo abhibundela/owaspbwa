32b9daa83d8e03552d64fbfaf4af206ebcb8de5a
michaeltcyr@gmail.com <michaeltcyr@gmail.com@0a5599c8-c0cf-11de-a250-c7f789e21d83>
2011-05-03 19:21:19 +0000

    Fixed mysql chmod 700 issue

--- trunk/usr/local/bin/owaspbwa-fix-file-permissions.sh
+++ trunk/usr/local/bin/owaspbwa-fix-file-permissions.sh
@@ -5,6 +5,7 @@ chown -R www-data:www-data /owaspbwa/owaspbwa-svn/var/www
 chown -R www-data:www-data /owaspbwa/owaspbwa-svn/var/lib/awstats
 echo ".: Fixing MySQL Permissions:."
 chown -R mysql:mysql /owaspbwa/owaspbwa-svn/var/lib/mysql
+chmod 700 /owaspbwa/owaspbwa-svn/var/lib/mysql/main/
 echo ".: Fixing PostgreSQL Permissions:."
 chown -R postgres:postgres /owaspbwa/owaspbwa-svn/var/lib/postgresql
 echo ".: Fixing Tomcat Permissions :."
