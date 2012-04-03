1aa4a41c24712e0373a2b88f4c384417669a440c
chuck.f.willis <chuck.f.willis@gmail.com>
2012-04-03 20:15:32 +0000

    Updated scripts.

--- usr/local/bin/owaspbwa-update-all.sh
+++ usr/local/bin/owaspbwa-update-all.sh
@@ -16,27 +16,29 @@ echo "---- Stopping services ----"
 owaspbwa-services-stop.sh
 
 echo "---- Updating from OWASP BWA SVN Repo ----"
+# Use --accept theirs-full particularly because database files may have been 
+# altered locally and we are okay with overwriting those with what is in SVN.
 svn update --accept theirs-full /owaspbwa/owaspbwa-svn
 if [ "$?"-ne 0]; then echo "SVN Update from OWASP BWA SVN Repo Failed!"; exit 1; fi 
 
 echo "---- Updating from DVWA SVN Repo ----"
-svn update --accept theirs-full /owaspbwa/dvwa-svn
+svn update /owaspbwa/dvwa-svn
 if [ "$?"-ne 0]; then echo "SVN Update from DVWA SVN Repo Failed!"; exit 1; fi 
 
 echo "---- Updating from OWASP ZAP WAVE SVN Repo ----"
-svn update --accept theirs-full /owaspbwa/owasp-zap-wave-svn
+svn update /owaspbwa/owasp-zap-wave-svn
 if [ "$?"-ne 0]; then echo "SVN Update from OWASP ZAP WAVE SVN Repo Failed!"; exit 1; fi 
 
 echo "---- Updating from ModSecurity Core Rule Set SVN Repo ----"
-svn update --accept theirs-full /owaspbwa/modsecurity-crs-svn
+svn update /owaspbwa/modsecurity-crs-svn
 if [ "$?"-ne 0]; then echo "SVN Update from ModSecurity Core Rule Set SVN Repo Failed!"; exit 1; fi 
 
 echo "---- Updating from BodgeIt SVN Repo ----"
-svn update --accept theirs-full /owaspbwa/bodgeit-svn
+svn update /owaspbwa/bodgeit-svn
 if [ "$?"-ne 0]; then echo "SVN Update from BodgeIt SVN Repo Failed!"; exit 1; fi 
 
 echo "---- Updating from OWASP WebGoat (Java) SVN Repo ----"
-svn update --accept theirs-full /owaspbwa/WebGoat-svn
+svn update /owaspbwa/WebGoat-svn
 if [ "$?"-ne 0]; then echo "SVN Update from OWASP WebGoat (Java) SVN Repo Failed!"; exit 1; fi 
 # build and deploy new version of WebGoat
 echo "Compiling and deploying WebGoat in /owaspbwa/WebGoat-svn...." 
@@ -45,7 +47,7 @@ mvn tomcat:undeploy ; mvn compile ; mvn war:exploded tomcat:exploded
 cd - # return to previous directory
 
 echo "---- Updating from OWASP ESAPI Java SwingSet SVN Repo ----"
-svn update --accept theirs-full /owaspbwa/owasp-esapi-java-swingset-svn
+svn update /owaspbwa/owasp-esapi-java-swingset-svn
 echo "Compiling and deploying OWASP ESAPI Java SwingSet in /owaspbwa/owasp-esapi-java-swingset-svn...." 
 # build and deploy new version of OWASP ESAPI Java SwingSet 
 cd /owaspbwa/owasp-esapi-java-swingset-svn
@@ -53,7 +55,7 @@ mvn tomcat:undeploy ; mvn compile ; mvn war:exploded tomcat:exploded
 cd - # return to previous directory
 
 echo "---- Updating from OWASP ESAPI Java SwingSet Interactive SVN Repo ----"
-svn update --accept theirs-full /owaspbwa/owasp-esapi-java-swingset-interactive-svn
+svn update /owaspbwa/owasp-esapi-java-swingset-interactive-svn
 echo "Compiling and deploying OWASP ESAPI Java SwingSet Interactive in /owaspbwa/owasp-esapi-java-swingset-interactive-svn...." 
 # build and deploy new version of OWASP ESAPI Java SwingSet Interactive
 cd /owaspbwa/owasp-esapi-java-swingset-interactive-svn/SwingSet
