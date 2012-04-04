ef594e4c22bf8e6645d47f21fb8969d95a60c1d8
chuck.f.willis <chuck.f.willis@gmail.com>
2012-04-04 02:05:12 +0000

    Fixed error handling in update scripts.

--- usr/local/bin/owaspbwa-update-owaspbwa-only.sh
+++ usr/local/bin/owaspbwa-update-owaspbwa-only.sh
@@ -15,7 +15,7 @@ echo "---- Stopping services ----"
 owaspbwa-services-stop.sh
 echo "---- Updating from OWASP BWA SVN Repo ----"
 svn update --accept theirs-full /owaspbwa/owaspbwa-svn
-if [ "$?"-ne 0]; then echo "SVN Update Failed!"; exit 1; fi 
+if [ "$?" -ne 0 ] ; then echo "SVN Update Failed!"; exit 1; fi 
 echo "---- Fixing file permissions and restarting services ----"
 #when we update the scripts in /usr/local/bin, it may break the permissions
 chmod +x /owaspbwa/owaspbwa-svn/usr/local/bin/*.sh
