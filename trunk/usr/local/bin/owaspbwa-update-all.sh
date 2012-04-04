fd29b287584ecfa97981e4965473fab287e80f7f
chuck.f.willis@gmail.com <chuck.f.willis@gmail.com@0a5599c8-c0cf-11de-a250-c7f789e21d83>
2012-04-04 02:05:12 +0000

    Fixed error handling in update scripts.

--- trunk/usr/local/bin/owaspbwa-update-all.sh
+++ trunk/usr/local/bin/owaspbwa-update-all.sh
@@ -19,27 +19,27 @@ echo "---- Updating from OWASP BWA SVN Repo ----"
 # Use --accept theirs-full particularly because database files may have been 
 # altered locally and we are okay with overwriting those with what is in SVN.
 svn update --accept theirs-full /owaspbwa/owaspbwa-svn
-if [ "$?"-ne 0]; then echo "SVN Update from OWASP BWA SVN Repo Failed!"; exit 1; fi 
+if [ "$?" -ne 0 ] ; then echo "SVN Update from OWASP BWA SVN Repo Failed!"; exit 1; fi 
 
 echo "---- Updating from DVWA SVN Repo ----"
 svn update /owaspbwa/dvwa-svn
-if [ "$?"-ne 0]; then echo "SVN Update from DVWA SVN Repo Failed!"; exit 1; fi 
+if [ "$?" -ne 0 ] ; then echo "SVN Update from DVWA SVN Repo Failed!"; exit 1; fi 
 
 echo "---- Updating from OWASP ZAP WAVE SVN Repo ----"
 svn update /owaspbwa/owasp-zap-wave-svn
-if [ "$?"-ne 0]; then echo "SVN Update from OWASP ZAP WAVE SVN Repo Failed!"; exit 1; fi 
+if [ "$?" -ne 0 ] ; then echo "SVN Update from OWASP ZAP WAVE SVN Repo Failed!"; exit 1; fi 
 
 echo "---- Updating from ModSecurity Core Rule Set SVN Repo ----"
 svn update /owaspbwa/modsecurity-crs-svn
-if [ "$?"-ne 0]; then echo "SVN Update from ModSecurity Core Rule Set SVN Repo Failed!"; exit 1; fi 
+if [ "$?" -ne 0 ] ; then echo "SVN Update from ModSecurity Core Rule Set SVN Repo Failed!"; exit 1; fi 
 
 echo "---- Updating from BodgeIt SVN Repo ----"
 svn update /owaspbwa/bodgeit-svn
-if [ "$?"-ne 0]; then echo "SVN Update from BodgeIt SVN Repo Failed!"; exit 1; fi 
+if [ "$?" -ne 0 ] ; then echo "SVN Update from BodgeIt SVN Repo Failed!"; exit 1; fi 
 
 echo "---- Updating from OWASP WebGoat (Java) SVN Repo ----"
 svn update /owaspbwa/WebGoat-svn
-if [ "$?"-ne 0]; then echo "SVN Update from OWASP WebGoat (Java) SVN Repo Failed!"; exit 1; fi 
+if [ "$?" -ne 0 ] ; then echo "SVN Update from OWASP WebGoat (Java) SVN Repo Failed!"; exit 1; fi 
 # build and deploy new version of WebGoat
 echo "Compiling and deploying WebGoat in /owaspbwa/WebGoat-svn...." 
 cd /owaspbwa/WebGoat-svn
@@ -48,6 +48,7 @@ cd - # return to previous directory
 
 echo "---- Updating from OWASP ESAPI Java SwingSet SVN Repo ----"
 svn update /owaspbwa/owasp-esapi-java-swingset-svn
+if [ "$?" -ne 0 ] ; then echo "SVN Update from OWASP ESAPI Java SwingSet SVN Repo Failed!"; exit 1; fi 
 echo "Compiling and deploying OWASP ESAPI Java SwingSet in /owaspbwa/owasp-esapi-java-swingset-svn...." 
 # build and deploy new version of OWASP ESAPI Java SwingSet 
 cd /owaspbwa/owasp-esapi-java-swingset-svn
@@ -56,6 +57,7 @@ cd - # return to previous directory
 
 echo "---- Updating from OWASP ESAPI Java SwingSet Interactive SVN Repo ----"
 svn update /owaspbwa/owasp-esapi-java-swingset-interactive-svn
+if [ "$?" -ne 0 ] ; then echo "SVN Update from OWASP ESAPI Java SwingSet Interactive SVN Repo Failed!"; exit 1; fi 
 echo "Compiling and deploying OWASP ESAPI Java SwingSet Interactive in /owaspbwa/owasp-esapi-java-swingset-interactive-svn...." 
 # build and deploy new version of OWASP ESAPI Java SwingSet Interactive
 cd /owaspbwa/owasp-esapi-java-swingset-interactive-svn/SwingSet
@@ -65,13 +67,13 @@ cd - # return to previous directory
 echo "---- Updating from webgoat.net GIT Repo ----"
 cd /owaspbwa/webgoat.net-git
 git pull 
-if [ "$?"-ne 0]; then echo "GIT Update from webgoat.net GIT Repo Failed!"; exit 1; fi 
+if [ "$?" -ne 0 ] ; then echo "GIT Update from webgoat.net GIT Repo Failed!"; exit 1; fi 
 cd - # return to previous directory
 
 echo "---- Updating from WackoPicko GIT Repo ----"
 cd /owaspbwa/WackoPicko-relative_urls-git
 git pull 
-if [ "$?"-ne 0]; then echo "GIT Update from WackoPicko GIT Repo Failed!"; exit 1; fi 
+if [ "$?" -ne 0 ] ; then echo "GIT Update from WackoPicko GIT Repo Failed!"; exit 1; fi 
 cd - # return to previous directory
 
 echo "---- Fixing file permissions and restarting services ----"
