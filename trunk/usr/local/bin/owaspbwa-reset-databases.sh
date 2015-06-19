3a5243b33cc43316b95d8732fa4d04dc20d1534f
chuck.f.willis@gmail.com <chuck.f.willis@gmail.com@0a5599c8-c0cf-11de-a250-c7f789e21d83>
2015-06-19 04:47:15 +0000

    Integrated Security Shepherd

--- trunk/usr/local/bin/owaspbwa-reset-databases.sh
+++ trunk/usr/local/bin/owaspbwa-reset-databases.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Reseting databases for applications that have automated capbility to do so."
-echo "Currently: Mutillidae, bWAPP, DVWA, and OWASP Bricks"
+echo "Currently: Mutillidae, bWAPP, DVWA, OWASP Bricks, MCIR (sqlol), and Security Shepherd"
 
 #reset the mutillidae database
 wget -qO- http://127.0.0.1/mutillidae/set-up-database.php &> /dev/null
@@ -11,10 +11,17 @@ wget -qO- http://127.0.0.1/bWAPP/reset.php?secret=bWAPP &> /dev/null
 #reset the OWASP Bricks database
 wget -qO- http://127.0.0.1/owaspbricks/config/setup.php  &> /dev/null
 
-# reset the DVWA database
+#reset the DVWA database
 wget -qO- http://127.0.0.1/dvwa/setup.php --post-data="create_db=Create+%2F+Reset+Database" &> /dev/null
 
 #reset the MCIR sqlol database
 wget -qO- http://127.0.0.1/MCIR/sqlol/resetbutton.php  &> /dev/null
+
+#reset the security shepherd database
+cat /owaspbwa/SecurityShepherd-git/SecurityShepherdCore/database/dropAndRecreateCoreDB.sql | mysql --user="securityshepherd" --password="securityshepherd"
+cat /owaspbwa/SecurityShepherd-git/SecurityShepherdCore/database/SHA*.sql | mysql --user="securityshepherd" --password="securityshepherd" --database="core"
+cat /owaspbwa/SecurityShepherd-git/SecurityShepherdCore/database/coreSchema.sql | mysql --user="securityshepherd" --password="securityshepherd"
+cat /owaspbwa/SecurityShepherd-git/SecurityShepherdCore/database/moduleSchemas.sql | mysql --user="securityshepherd" --password="securityshepherd"
+
 
 
